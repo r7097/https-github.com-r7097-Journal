93cc488f1ec7a7b3f39a40958b2d833ea7b75c53
Raghad <r70974284@gmail.com>
2025-10-26 14:37:15 +0300

    Initial Commit

---
+++ Journali/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Journali
+//
+//  Created by Raghad Alamoudi on 04/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Raghad Alamoudi on 04/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
